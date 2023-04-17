472e1a451f9869f46b47000fd7497d9402fe4236
jose-almir <jr.cod.dev@gmail.com>
2023-04-17 12:03:00 -0300

    Conexão com banco de dados criada

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// Vai disponibilizar o uso de variáveis de ambiente
+// Importações principais e variáveis de ambiente
 require("dotenv").config();
+const express = require("express");
+
+// Configuração do App
+const app = express();
+app.use(express.json()); // Possibilitar transitar dados usando JSON
+
+// Configuração do Banco de Dados
+const { connection, authenticate } = require("./database/database");
+authenticate(connection); // efetivar a conexão
+
+// Definição de rotas
+
+// Escuta de eventos (listen)
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000/");
+});
