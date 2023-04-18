5e32c132638fd19ee87a1bdd7e66c808ef318d81
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-04-18 12:04:26 -0300

    update e delete cliente

--- index.js
+++ index.js
@@ -52,6 +52,58 @@ app.post("/clientes", async (req, res) => {
   }
 });
 
+
+// atualizar um cliente
+app.put("/clientes/:id", async (req, res) => {
+  // obter dados do corpo da requisão
+  const { nome, email, telefone, endereco } = req.body;
+  // obter identificação do cliente pelos parametros da rota
+  const { id } = req.params;
+  try {
+    // buscar cliente pelo id passado
+    const cliente = await Cliente.findOne({ where: { id } });
+    // validar a existência desse cliente no banco de dados
+    if(cliente) {
+      // validar a existência desse do endereço passdo no corpo da requisição
+      if(endereco) {
+        await Endereco.update(endereco, { where: { clienteId: id } });
+      }
+      // atualizar o cliente com nome, email e telefone
+      await cliente.update({nome, email, telefone});
+      res.status(200).json({ message: "Cliente editado." });
+    }
+    else {
+      res.status(404).json({ message: "Cliente não encontrado." });
+    }
+  }
+  catch(err) {
+    console.error(err)
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+// excluir um cliente
+app.delete("/clientes/:id", async (req, res) => {
+  // obter identificação do cliente pela rota
+  const { id } = req.params;
+  // buscar cliente por id
+  const cliente = await Cliente.findOne({ where: { id } });
+  try {
+    if(cliente) {
+      await cliente.destroy();
+      res.status(200).json({ message: "Cliente removido." });
+    }
+    else {
+      res.status(404).json({ message: "Cliente não encontrado." });
+    }
+  }
+  catch(err) {
+    console.error(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+
 // Escuta de eventos (listen)
 app.listen(3000, () => {
   // Gerar as tabelas a partir do model
